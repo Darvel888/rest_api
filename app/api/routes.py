00605b3df6f32386168fc207f7acd25e8542c478
Dasha Dubovik <daashka0802@gmail.com>
2019-12-14 23:28:08 +0600

    Added standarts methods for Bookings table

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,6 +5,7 @@ from app.models import Aircrafts_data, Aircrafts_data_Schema, Airports_data, Air
     Boarding_passes, Boarding_passes_Schema, Bookings, Bookings_Schema, Flights, Flights_Schema, \
         Seats, Seats_Schema, Ticket_flights, Ticket_flights_Schema, Tickets, Tickets_Schema
 from app.api import bp
+from app.api.errors import error_response
 
 #initializing ma.Schemas
 Aircraft_data_s = Aircrafts_data_Schema()
@@ -28,10 +29,55 @@ Tickets_s = Tickets_Schema(many=True)
 def index():
     return jsonify({'msg': 'Hello World!'})
 
-@bp.route('/bookings', methods=['GET'])
-def get_bookings():
-    all_bookings = Bookings.query.all()
-    return Bookings_s.dump(all_bookings) #не работает из за слишком большого объёма данных
+@bp.route('/bookings/<count>', methods=['GET'])
+#Outputs last <count> of table 'Bookings'
+def get_bookings(count):
+    try:
+        count = int(count)
+    except:
+        return error_response(400)
+    if(type(count) != int or count <= 0):
+        return error_response(404)
+    all_bookings = Bookings.query.limit(count).all()
+    return jsonify(Bookings_s.dump(all_bookings))
+
+@bp.route('/bookings/book_ref/<book_ref>', methods=['GET'])
+#Outputs <book_ref> of table 'Bookings'
+def get_single_bookings(book_ref):
+    booking = Bookings.query.get_or_404(book_ref)
+    return jsonify(Booking_s.dump(booking))
+
+@bp.route('/bookings', methods=['POST'])
+#Adds a new booking into 'Bookings'
+def add_booking():
+    book_ref = request.json['book_ref']
+    book_date = request.json['book_date']
+    total_amount = request.json['total_amount']
+    booking = Bookings(book_ref, book_date, total_amount)
+    db.session.add(booking)
+    db.session.commit()
+    return Booking_s.jsonify(booking)
+
+@bp.route('/bookings/book_ref/<book_ref>', methods=['DELETE'])
+#Delete a row with <book_ref> pk from table 'Booking'
+def delete_booking(book_ref):
+    booking = Bookings.query.get(book_ref)
+    db.session.delete(booking)
+    db.session.commit()
+    return Booking_s.jsonify(booking)
+
+@bp.route('/bookings/book_ref/<book_ref>', methods=['PUT'])
+#edits <book_ref> of table 'Bookings'
+def update_booking(book_ref):
+    booking = Bookings.query.get_or_404(book_ref)
+    book_ref = request.json['book_ref']
+    book_date = request.json['book_date']
+    total_amount = request.json['total_amount']
+    booking.book_ref = book_ref
+    booking.book_date = book_date
+    booking.total_amount = total_amount
+    db.session.commit()
+    return Booking_s.jsonify(booking)
 
 @bp.route('/aircrafts', methods=['GET'])
 def get_aircrafts():
